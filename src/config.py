79e1971716865dad2d1524df421f1ae88b445de5
Kyryl Bedrak <kyrylobedrak@gmail.com>
2023-10-31 00:45:18 +0200

    cor

--- src/config.py
+++ src/config.py
@@ -5,9 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT")
-DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
 JWT_SECRET = os.environ.get("JWT_SECRET")
-JWT_ALGORITHM = os.environ.get("JWT_ALGORITHM")
+
+DB_PORT = 5432
+DB_NAME = "cinema"
+DB_USER = "pgadmin"
+JWT_ALGORITHM = "HS256"
